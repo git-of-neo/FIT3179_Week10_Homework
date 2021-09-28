e621aebc87a231722fb6c1856ea9bcae672682aa
Neo <64857033+git-of-neo@users.noreply.github.com>
2021-09-28 21:00:33 +0800

    Finished all tasks

--- js/viz.js
+++ js/viz.js
@@ -1,9 +1,9 @@
 var box = "js/dot_chart.vg.json";
-    vegaEmbed("#dot_chart", box, {"actions":false}).then(function(result) {
+    vegaEmbed("#dot_chart", box, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var box = "js/bar_chart.vg.json";
-    vegaEmbed("#bar_chart", box, {"actions":false}).then(function(result) {
+var bar = "js/bar_chart.vg.json";
+    vegaEmbed("#bar_chart", bar, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
